7a4feb2861da256436584e37c324becf59167cc6
xuzhiwei <jsrdxzw@163.com>
2022-07-19 11:02:30 +0800

    init project

---
+++ src/main/java/com/jsrdxzw/salesupport/SaleSupportApplication.java
@@ -0,0 +1,13 @@
+package com.jsrdxzw.salesupport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaleSupportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaleSupportApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jsrdxzw/salesupport/SaleSupportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsrdxzw.salesupport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaleSupportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
